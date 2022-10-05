8ea478d91759bca2bf4f4c530ed41ffa98dd32e0
djphair <108026519+djphair207@users.noreply.github.com>
2022-10-05 06:16:55 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Unit5Modules-1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+#include "unistd.h"
+
+using namespace std;
+
+void PrintVec(vector<int> myVec){
+	for(int i = 0; i < myVec.size(); i++){
+		if(i != myVec.size() - 1){
+			cout << myVec[i] << ", ";
+		}
+		else{
+			cout << myVec[i];
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+  cout << "Hello World!" << endl;
+
+	vector<int> randVec({23, 14, 65, 3, 19, 2, 71, 12, 8, 61, 5, 25});
+	
+	for(int i = 0; i < randVec.size() - 1; i ++){
+		PrintVec(randVec);
+		cout <<"* * * i: "<< i << " * * *" << endl;
+		int min = i;
+		for(int j = i+1; j < randVec.size(); j++){
+			cout <<"j: "<< j << endl;
+			if(randVec[j] < randVec[min]){
+				min = j;
+			}
+			swap(randVec[i],randVec[min]);
+		}
+	}
+
+	PrintVec(randVec);
+	
+}
